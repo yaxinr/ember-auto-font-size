471338bbadd6be256dfbaaad6c22535ff401bae4
yaxinr <yaxinr@gmail.com>
2019-08-16 19:48:26 +0500

    performance

--- addon/components/auto-font-size.js
+++ addon/components/auto-font-size.js
@@ -4,24 +4,27 @@ import layout from '../templates/components/auto-font-size';
 export default Component.extend({
   layout,
   didRender: function() {
-    var fontSize, maxHeight, ourText, textHeight;
+    var fontSize, maxHeight, ourText;
     this._super(...arguments);
     this.element.style.setProperty('background-color', this.bgColor, 'important');
     if (this.bgColor === '#ffffff') {
       this.element.style.setProperty('color', 'black', 'important');
     }
-    fontSize = Number(this.maxFontPixels || 999);
-    ourText = this.element.children[0];
     maxHeight = this.element.offsetHeight;
+    fontSize = Number(this.maxFontPixels || maxHeight);
+    ourText = this.element.children[0];
     ourText.style.width = '100%';
     ourText.style.overflowWrap = 'break-word';
-    while (true) {
+    var minFS = 3, maxFS = fontSize;
+    while (fontSize != minFS) {
       ourText.style.fontSize = `${fontSize}px`;
-      textHeight = ourText.offsetHeight;
-      fontSize = fontSize - 1;
-      if ((textHeight < maxHeight) || fontSize <= 3) {
-        break;
+      if (ourText.offsetHeight < maxHeight) {
+        minFS = fontSize;
+      } else{
+        maxFS = fontSize;
       }
+      fontSize = Math.floor((minFS + maxFS)/2);
     }
+    ourText.style.fontSize = `${minFS}px`;
   }
 });
--- addon/components/text-fill.js
+++ addon/components/text-fill.js
@@ -4,24 +4,27 @@ import layout from '../templates/components/auto-font-size';
 export default Component.extend({
   layout,
   didRender: function() {
-    var fontSize, maxHeight, el, textHeight;
+    var fontSize, maxHeight, el;
     this._super(...arguments);
     el = this.element;
     el.style.setProperty('background-color', this.bgColor, 'important');
     if (this.bgColor === '#ffffff') {
       el.style.setProperty('color', 'black', 'important');
     }
-    fontSize = Number(this.maxFontPixels || 999);
     maxHeight = el.parentElement.offsetHeight;
+    fontSize = Number(this.maxFontPixels || maxHeight);
     el.style.width = '100%';
     el.style.overflowWrap = 'break-word';
-    while (true) {
+    var minFS = 3, maxFS = fontSize;
+    while (fontSize != minFS) {
       el.style.fontSize = `${fontSize}px`;
-      textHeight = el.offsetHeight;
-      fontSize = fontSize - 1;
-      if ((textHeight < maxHeight) || fontSize <= 3) {
-        break;
+      if (el.offsetHeight < maxHeight) {
+        minFS = fontSize;
+      } else{
+        maxFS = fontSize;
       }
+      fontSize = Math.floor((minFS + maxFS)/2);
     }
+    el.style.fontSize = `${minFS}px`;
   }
 });
