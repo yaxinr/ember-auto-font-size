c41608e6e5a333a48a96bcb41291b4180ffd1077
gilfanovrrPaker <gilfanovrr@npf-paker.ru>
2019-10-18 13:51:56 +0500

    clientWidth as limit

--- addon/components/auto-font-size.js
+++ addon/components/auto-font-size.js
@@ -4,27 +4,27 @@ import layout from '../templates/components/auto-font-size';
 export default Component.extend({
   layout,
   didRender: function() {
-    var fontSize, maxHeight, ourText;
+    var fontSize, maxHeight, maxWidth, textElement, parentElement;
     this._super(...arguments);
-    this.element.style.setProperty('background-color', this.bgColor, 'important');
+    textElement = this.element.children[0];
+    parentElement = textElement.parentElement;
+    parentElement.style.setProperty('background-color', this.bgColor, 'important');
     if (this.bgColor === '#ffffff') {
-      this.element.style.setProperty('color', 'black', 'important');
+      parentElement.style.setProperty('color', 'black', 'important');
     }
-    maxHeight = this.element.offsetHeight;
+    maxHeight = parentElement.clientHeight;
+    maxWidth = parentElement.clientWidth;
     fontSize = Number(this.maxFontPixels || maxHeight);
-    ourText = this.element.children[0];
-    ourText.style.width = '100%';
-    ourText.style.overflowWrap = 'break-word';
     var minFS = 3, maxFS = fontSize;
     while (fontSize != minFS) {
-      ourText.style.fontSize = `${fontSize}px`;
-      if (ourText.offsetHeight < maxHeight) {
+      textElement.style.fontSize = `${fontSize}px`;
+      if (textElement.offsetHeight < maxHeight && textElement.offsetWidth <= maxWidth) {
         minFS = fontSize;
       } else{
         maxFS = fontSize;
       }
       fontSize = Math.floor((minFS + maxFS)/2);
     }
-    ourText.style.fontSize = `${minFS}px`;
+    textElement.style.fontSize = `${minFS}px`;
   }
 });
--- addon/components/text-fill.js
+++ addon/components/text-fill.js
@@ -4,27 +4,27 @@ import layout from '../templates/components/auto-font-size';
 export default Component.extend({
   layout,
   didRender: function() {
-    var fontSize, maxHeight, el;
+    var fontSize, maxHeight, maxWidth, textElement, parentElement;
     this._super(...arguments);
-    el = this.element;
-    el.style.setProperty('background-color', this.bgColor, 'important');
+    textElement = this.element;
+    parentElement = textElement.parentElement;
+    parentElement.style.setProperty('background-color', this.bgColor, 'important');
     if (this.bgColor === '#ffffff') {
-      el.style.setProperty('color', 'black', 'important');
+      parentElement.style.setProperty('color', 'black', 'important');
     }
-    maxHeight = el.parentElement.offsetHeight;
+    maxHeight = parentElement.clientHeight;
+    maxWidth = parentElement.clientWidth;
     fontSize = Number(this.maxFontPixels || maxHeight);
-    el.style.width = '100%';
-    el.style.overflowWrap = 'break-word';
     var minFS = 3, maxFS = fontSize;
     while (fontSize != minFS) {
-      el.style.fontSize = `${fontSize}px`;
-      if (el.offsetHeight < maxHeight) {
+      textElement.style.fontSize = `${fontSize}px`;
+      if (textElement.offsetHeight < maxHeight && textElement.offsetWidth <= maxWidth) {
         minFS = fontSize;
       } else{
         maxFS = fontSize;
       }
       fontSize = Math.floor((minFS + maxFS)/2);
     }
-    el.style.fontSize = `${minFS}px`;
+    textElement.style.fontSize = `${minFS}px`;
   }
 });
