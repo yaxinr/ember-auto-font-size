4cc615f4f1cc36b925b0062f3fe0d96b61e9085f
Gilfanov Ramil <yaxinr@gmail.com>
2022-06-06 09:55:00 +0500

    change padding and margin

--- addon/components/auto-font-size.js
+++ addon/components/auto-font-size.js
@@ -1,30 +1,47 @@
-import Component from '@ember/component';
-import layout from '../templates/components/auto-font-size';
-
+import Component from "@ember/component";
+import layout from "../templates/components/auto-font-size";
+import { debug } from "@ember/debug";
 export default Component.extend({
   layout,
-  didRender: function() {
-    var fontSize, maxHeight, maxWidth, textElement, parentElement;
+  didRender: function () {
     this._super(...arguments);
-    textElement = this.element.children[0];
-    parentElement = textElement.parentElement;
-    parentElement.style.setProperty('background-color', this.bgColor, 'important');
-    if (this.bgColor === '#ffffff') {
-      parentElement.style.setProperty('color', 'black', 'important');
-    }
-    maxHeight = parentElement.clientHeight;
-    maxWidth = parentElement.clientWidth;
-    fontSize = Number(this.maxFontPixels || maxHeight);
-    var minFS = 3, maxFS = fontSize;
-    while (fontSize != minFS) {
-      textElement.style.fontSize = `${fontSize}px`;
-      if (textElement.offsetHeight < maxHeight && textElement.offsetWidth <= maxWidth) {
-        minFS = fontSize;
-      } else{
-        maxFS = fontSize;
-      }
-      fontSize = Math.floor((minFS + maxFS)/2);
-    }
-    textElement.style.fontSize = `${minFS}px`;
-  }
+    const textElement = this.element.children[0];
+    setFontSize(textElement, this.bgColor, this.maxFontPixels);
+  },
 });
+export function setFontSize(textElement, bgColor, maxFontPixels) {
+  const parentElement = textElement.parentElement;
+  parentElement.style.setProperty("background-color", bgColor, "important");
+  if (bgColor === "#ffffff") {
+    parentElement.style.setProperty("color", "black", "important");
+  }
+  const parentClientHeight = parentElement.clientHeight;
+  const parentClientWidth = parentElement.clientWidth;
+  textElement.style.padding = "unset";
+  textElement.style.margin = "auto";
+  debug(
+    `parentElement: maxHeight=${parentClientHeight} maxWidth=${parentClientWidth}`
+  );
+  let fontSize = Number(maxFontPixels || parentClientHeight);
+  var minFS = 3,
+    maxFS = fontSize;
+  debug(`fontSize=${fontSize} minFS=${minFS} minFS=${maxFS}`);
+  while (fontSize != minFS) {
+    textElement.style.fontSize = `${fontSize}px`;
+    debug(
+      `textElement: scrollHeight=${parentElement.scrollHeight} scrollWidth=${parentElement.scrollWidth}`
+    );
+    if (
+      parentElement.scrollHeight <= parentClientHeight &&
+      parentElement.scrollWidth <= parentClientWidth
+    ) {
+      minFS = fontSize;
+    } else {
+      maxFS = fontSize;
+    }
+    fontSize = Math.floor((minFS + maxFS) / 2);
+    debug(`fontSize=${fontSize} minFS=${minFS} minFS=${maxFS}`);
+  }
+  textElement.style.fontSize = `${minFS}px`;
+  debug(`textElement.style.fontSize = ${minFS}px`);
+}
--- addon/components/text-fill.js
+++ addon/components/text-fill.js
@@ -1,30 +1,10 @@
-import Component from '@ember/component';
-import layout from '../templates/components/auto-font-size';
-
+import Component from "@ember/component";
+import layout from "../templates/components/auto-font-size";
+import { setFontSize } from "./auto-font-size";
 export default Component.extend({
   layout,
-  didRender: function() {
-    var fontSize, maxHeight, maxWidth, textElement, parentElement;
+  didRender: function () {
     this._super(...arguments);
-    textElement = this.element;
-    parentElement = textElement.parentElement;
-    parentElement.style.setProperty('background-color', this.bgColor, 'important');
-    if (this.bgColor === '#ffffff') {
-      parentElement.style.setProperty('color', 'black', 'important');
-    }
-    maxHeight = parentElement.clientHeight;
-    maxWidth = parentElement.clientWidth;
-    fontSize = Number(this.maxFontPixels || maxHeight);
-    var minFS = 3, maxFS = fontSize;
-    while (fontSize != minFS) {
-      textElement.style.fontSize = `${fontSize}px`;
-      if (textElement.offsetHeight < maxHeight && textElement.offsetWidth <= maxWidth) {
-        minFS = fontSize;
-      } else{
-        maxFS = fontSize;
-      }
-      fontSize = Math.floor((minFS + maxFS)/2);
-    }
-    textElement.style.fontSize = `${minFS}px`;
-  }
+    setFontSize(this.element, this.bgColor, this.maxFontPixels);
+  },
 });
